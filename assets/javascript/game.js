571e84bbf4ab523fd8d897698b819b05e5f3c354
relosaj <john.relosa@me.com>
2017-05-12 00:20:30 -0700

    skeletons

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+
+var wins = 0
+var losses =0
+
+var guessesLeft = 9
+
+var guessesMade=0
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+// computer's pick
+
+
+
+// user's pick
+
+
+// win condition
+
+
+// subtract number of guesses
+
+
+// user's guesses so far
