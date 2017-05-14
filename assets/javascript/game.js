970a3908d6a95eb618040d5e0ddde036c25bfc06
relosaj <john.relosa@me.com>
2017-05-14 16:44:40 -0700

    close but bad logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,52 @@
-
-
-
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
+// var computersPick = "";
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// var computersPick = letters[Math.floor(Math.random() * letters.length)];
+// 	console.log("This is the target: " + computersPick);
+
+function newLetter() {
+	var newHiddenLetter = letters[Math.floor(Math.random() * letters.length)];
+	// var newHiddenLetter = computersPick;
+	console.log("This is the target: " + newHiddenLetter);
+}
 
 var computersPick = letters[Math.floor(Math.random() * letters.length)];
 	console.log("This is the target: " + computersPick);
 
-function newLetter(mystic) {
-	var newHiddenLetter = letters[Math.floor(Math.random() * letters.length)];
-}
-
-
-
 document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("Key press: " + userGuess);
+	newLetter();
 
 	if (userGuess === computersPick) {
 		wins++;
 		console.log ("You've won");
-		console.log ("You've won: " + wins + " games!")
+		console.log ("You've won: " + wins + " games!");
 		guessesLeft = 9;
 		document.getElementById("userGuess").innerHTML = "";
-	} else
+		// newLetter();
+	} 
 	if (guessesLeft === 0) {
 		losses++;
-		console.log ("You've lost")
+		console.log ("You've lost");
 		console.log ("Games Lost: " + losses + " games");	
 		guessesLeft = 9;
 		document.getElementById("userGuess").innerHTML = "";
-	} else
+		// newLetter();
+	} 
 	if (userGuess != computersPick) {
 		guessesLeft--;
 		console.log("Try again. You have: " + guessesLeft + " guesses left!");
 	}
 
-
-
-
-
-
-
-
-
-
 	document.getElementById("wins").innerHTML = wins;
 	document.getElementById("losses").innerHTML = losses;
 	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	document.getElementById("userGuess").innerHTML += userGuess;
-
 }
 
 
